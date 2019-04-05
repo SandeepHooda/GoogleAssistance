371a0d011b8cf715315543a980f371883ce55e82
SandeepHooda <sonu.hooda@gmail.com>
2019-04-06 00:42:39 +0530

    first commit

--- src/ai/api/web/Test.java
+++ src/ai/api/web/Test.java
@@ -9,12 +9,18 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.gson.Gson;
+
+import ai.api.GsonFactory;
+import request.GoogleRequest;
+
 /**
  * Servlet implementation class Test
  */
 @WebServlet("/Test")
 public class Test extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	  private final Gson gson = GsonFactory.getDefaultFactory().getGson();
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -28,11 +34,20 @@ public class Test extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		 StringBuilder sb = new StringBuilder();
+         String s;
+         while ((s = request.getReader().readLine()) != null) {
+             sb.append(s);
+         }
+
+         GoogleRequest googlerequest = (GoogleRequest) gson.fromJson(sb.toString(), GoogleRequest.class);
+         
 		response.setContentType("application/json");
 		response.setCharacterEncoding("UTF-8");
 		PrintWriter out = response.getWriter();
 		String responseStr = "{\r\n" + 
-		"  \"fulfillmentText\": \"Sandeep this is a test web hook\",\r\n" + 
+		"  \"fulfillmentText\": \"You asked me to,  "+googlerequest.getQueryResult().getQueryText()+". Here deivice name is, " +googlerequest.getQueryResult().getParameters().get("DeviceName")+". \",\r\n" + 
 		"  \"outputContexts\": []\r\n" + 
 		"}";
         out.print(responseStr );
